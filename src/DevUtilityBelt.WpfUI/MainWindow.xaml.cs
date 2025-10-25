3e0bb4041bae9b872e9990641597d9de7f272fda
Junian Triajianto <author@junian.dev>
2025-10-25 22:19:38 +0700

    Use fluent UI

--- src/DevUtilityBelt.WpfUI/MainWindow.xaml.cs
+++ src/DevUtilityBelt.WpfUI/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ namespace DevUtilityBelt.WpfUI
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : FluentWindow
     {
         public MainWindow()
         {
