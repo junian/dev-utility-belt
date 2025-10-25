bf0ee1f0ca4287e7b45702352ace32e2c9fcce81
Junian Triajianto <author@junian.dev>
2025-10-26 05:52:50 +0700

    Add more dependencies

---
+++ src/DevUtilityBelt.Core/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevUtilityBelt.Core.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _appTitle = "Dev Utility Belt";
+    }
+}
