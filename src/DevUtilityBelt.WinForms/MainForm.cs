242ffd66f7608821cf49ec8de59e9ee0bf90b9a1
Junian Triajianto <author@junian.dev>
2025-10-26 06:03:13 +0700

    Add WinForms

---
+++ src/DevUtilityBelt.WinForms/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using DevUtilityBelt.Core.ViewModels;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DevUtilityBelt.WinForms
+{
+    public partial class MainForm : Form, IViewFor<MainViewModel>
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            ViewModel = new MainViewModel();
+            this.Bind(ViewModel, vm => vm.AppTitle, v => v.Text);
+        }
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public MainViewModel? ViewModel { get; set; }
+
+        object? IViewFor.ViewModel { get => ViewModel; set => ViewModel = (MainViewModel?) value; }
+    }
+}
